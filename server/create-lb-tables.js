8bcf95d840bda245d8f23c4a486afa5b2362619b
tengsun <tengsun@cn.ibm.com>
2018-05-28 11:15:56 +0800

    hook mysql db for auth

---
+++ server/create-lb-tables.js
@@ -0,0 +1,10 @@
+var server = require('./server');
+
+var ds = server.dataSources.mysql_db;
+var lbTables = ['User', 'AccessToken', 'ACL', 'Role', 'RoleMapping'];
+
+ds.automigrate(lbTables, function (er) {
+    if (er) throw er;
+    console.log('Loopback tables [' + lbTables + '] created in ' + ds.adapter.name);
+    ds.disconnect();
+});
