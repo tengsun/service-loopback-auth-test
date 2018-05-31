31986b8d9acb78f87ea4f55e30f45603a2370761
tengsun <tengsun@cn.ibm.com>
2018-05-31 16:06:11 +0800

    deny $everyone access, enable $auth to list ws

--- common/models/workspace.js
+++ common/models/workspace.js
@@ -2,4 +2,18 @@
 
 module.exports = function(Workspace) {
 
+    Workspace.list = function(cb) {
+        Workspace.find(function(err, workspaces) {
+            let nameList = [];
+            workspaces.forEach(ws => {
+                nameList.push(ws.name);
+            });
+            cb(null, nameList);
+        });
+    };
+
+    Workspace.remoteMethod('list', {
+        http: { path: '/list', verb: 'get' },
+        returns: { arg: 'workspaces', type: 'array' }
+    })
 };
